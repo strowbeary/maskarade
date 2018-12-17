d9ba5fb5537d6d9d47089ae7cef249283f230728
StrowBeary <remicaillot5@gmail.com>
2018-12-17 19:48:57 +0100

    multithread key gen

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
 use std::io::BufWriter;
-use pbr::ProgressBar;
-
+use std::thread;
+use std::sync::Arc;
+use std::sync::Mutex;
+use pbr::MultiBar;
 fn generate_prime() -> Int {
     let output = Command::new("openssl")
         .arg("prime")
@@ -27,10 +29,10 @@ fn generate_prime() -> Int {
 
 fn main()  {
     let length = 500000;
-    let mut pb = ProgressBar::new(length);
-    pb.format("║█░░║");
+
     let new_file = File::create(&Path::new("file.mask")).unwrap();
-    let mut stream = BufWriter::new(new_file);
+    let stream = BufWriter::new(new_file);
+    let arc_stream = Arc::new(Mutex::new(stream));
     print!("Generating two big primes");
     let m = generate_prime() * generate_prime();
     println!(" [done]");
@@ -42,19 +44,43 @@ fn main()  {
     }
     let x0 = seed.clone();
     println!(" [done]");
-
+    println!("Generating key");
     seed = x0.clone();
-    for _i in 0..length {
-        seed = seed.square().divmod(&m).1;
-        if seed.eq(&x0) {
-            println!("Period is {}", _i);
-        }
-        let mod_seed = seed.divmod(&Int::from(256)).1;
-        let bit = u8::from(&mod_seed);
 
-        stream.write(&[bit]).unwrap();
-        pb.inc();
+    let mut handles = vec![];
+    let mut mb = MultiBar::new();
+    mb.println("Generating key file");
+    let thread_number = 6;
+    for _offset in 0..thread_number {
+        let mut next_occurrence_offset = if _offset > 0 {1} else {0};
+        let start: u64 = length / thread_number * (_offset);
+        let end: u64 = (length / thread_number) * (_offset + 1) - 1;
+        println!("offset {}, from {} to {}", _offset, &start, &end);
+        let mut thread_seed = seed.clone();
+        let thread_m = m.clone();
+        let thread_x0 = seed.clone();
+        let thread_stream = Arc::clone(&arc_stream);
+        let mut pb = mb.create_bar(end - start);
+        pb.format("║█░░║");
+        let handle = thread::spawn( move || {
+            for _i in start..end {
+                thread_seed = thread_seed.square().divmod(&thread_m).1;
+                if thread_seed.eq(&thread_x0) {
+                    println!("Period is {}", _i);
+                }
+                let mod_seed = thread_seed.divmod(&Int::from(256)).1;
+                let bit = u8::from(&mod_seed);
+
+                thread_stream.lock().unwrap().write(&[bit]).unwrap();
+                pb.inc();
+            }
+            pb.finish();
+        });
+        handles.push(handle);
     }
-    pb.finish_print("done");
+    mb.listen();
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
 }
